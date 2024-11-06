e3f0c41118fa1309997ee0a2a21ba33922b0961d
Rohith Dileep <150414771+RohithDileep@users.noreply.github.com>
2024-11-06 23:27:51 +0530

    Create Stackkusinglinkedlist.c

---
+++ Stackkusinglinkedlist.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+int isEmpty(struct Node* top) {
+    return top == NULL;
+}
+void push(struct Node** top, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *top;
+    *top = newNode;
+    printf("%d pushed to stack\n", data);
+}
+int pop(struct Node** top) {
+    if (isEmpty(*top)) {
+        printf("Stack underflow\n");
+        return -1;
+    }
+    struct Node* temp = *top;
+    int popped = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    return popped;
+}
+void display(struct Node* top) {
+    if (isEmpty(top)) {
+        printf("Stack is empty\n");
+        return;
+    }
+    struct Node* temp = top;
+    printf("Stack elements: ");
+    while (temp != NULL) {
+        printf("%d ->", temp->data);
+        temp = temp->next;
+    }printf("NULL");
+    printf("\n");
+}
+int main() {
+    struct Node* top = NULL;
+    int choice, data;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter data to push: ");
+                scanf("%d", &data);
+                push(&top, data);
+                break;
+            case 2:
+                data = pop(&top);
+                if (data != -1) {
+                    printf("%d popped from stack\n", data);
+                }
+                break;
+            case 3:
+                display(top);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice, please try again.\n");
+        }
+    } while (choice != 4);
+    while (!isEmpty(top)) {
+        pop(&top);
+    }
+    return 0;
+}
